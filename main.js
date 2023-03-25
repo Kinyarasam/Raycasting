8d52fe039f28d56920e9528a86cb6eb69fd67a63
kinyarasam <skinyara.30@gmail.com>
2023-03-25 17:30:31 +0300

    configure tailwind css

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 #!/usr/bin/env node
-const app = getElementById("app");
-console.log(app);
+const app = document.getElementById("app");
+const context = app.getContext('2d');
+
+const HEIGHT = app.clientHeight;
+const WIDTH = app.clientWidth;
+
+const createRect = (x, y, width, height, color) => {
+  context.fillStyle = color;
+  context.fillRect(x, y, width, height);
+};
+
+createRect(
+  0, 0, WIDTH, HEIGHT, '#000000'
+)
