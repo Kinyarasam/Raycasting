5a1f05c492cbd87f961d10ec8de63e123a3cc441
kinyarasam <skinyara.30@gmail.com>
2023-03-25 17:41:29 +0300

    Code change

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const context = app.getContext('2d');
 
 const HEIGHT = app.clientHeight;
 const WIDTH = app.clientWidth;
+const FPS = 30;
 
 const createRect = (x, y, width, height, color) => {
   context.fillStyle = color;
@@ -13,3 +14,28 @@ const createRect = (x, y, width, height, color) => {
 createRect(
   0, 0, WIDTH, HEIGHT, '#000000'
 )
+
+const draw = () => {
+  /**
+   * TODO: Draw contents to the screen.
+   */
+  console.log('drawing to the screen');
+};
+
+const update = () => {
+  /**
+   * TODO: Update the contents on the screen
+   */ 
+  console.log('Updates');
+};
+
+const gameLoop = () => {
+  update();
+  draw();
+};
+
+const startLoop = () => {
+  setInterval(gameLoop, 1000 / FPS);
+}
+
+startLoop();
