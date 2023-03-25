f7f0d540b977f7831934fb20759dfd581520b8db
kinyarasam <skinyara.30@gmail.com>
2023-03-25 14:15:05 +0300

    Render static files

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,15 @@
  */
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+
+router.use(
+	express.static('.', { index: 'index.html' })
+);
 
 router.get('/', (req, res) => {
-  res.send('Home Route\n');
+  console.log('Home Route\n');
+	res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 module.exports = router;
